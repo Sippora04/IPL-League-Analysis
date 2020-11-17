429b677efcaa00e73a95531c72910ee06830b21b
Sippora04 <sippora.toppo95@gmail.com>
2020-11-18 02:53:41 +0530

    Added: get bowler with top bowling avg

--- main/java/com/ipl/IPLAnalyzer.java
+++ main/java/com/ipl/IPLAnalyzer.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.Gson;
 import com.ipl.IplAnalyzerException.ExceptionType;
@@ -14,15 +15,21 @@ import com.ipl.IplAnalyzerException.ExceptionType;
 public class IPLAnalyzer {
 
 	List<MostRunsCSV> list;
+	List<MostWicketsCSV> listBowler;
+	Map<SortType, Comparator<MostRunsCSV>> sortedMap;
 	ICSVBuilder csvBuilder = BuilderFactoryCSV.generateBuilder();
-
-	public IPLAnalyzer() {
-		list = new ArrayList<>();
-	}
 
 	public void loadIplData(String filePath) throws IplAnalyzerException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
 			list = csvBuilder.getList(reader, MostRunsCSV.class);
+		} catch (IOException e) {
+			throw new IplAnalyzerException("provided invalid file path", ExceptionType.INVALID_FILE_PATH);
+		}
+	}
+
+	public void loadIplDataBowler(String filePath) throws IplAnalyzerException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
+			listBowler = csvBuilder.getList(reader, MostWicketsCSV.class);
 		} catch (IOException e) {
 			throw new IplAnalyzerException("provided invalid file path", ExceptionType.INVALID_FILE_PATH);
 		}
@@ -36,23 +43,6 @@ public class IPLAnalyzer {
 		this.sortMostRunsCSV(csvComparator);
 		String sortedRunsList = new Gson().toJson(list);
 		return sortedRunsList;
-	}
-
-	private void sortMostRunsCSV(Comparator<MostRunsCSV> runsComparator) {
-		for (int i = 0; i < list.size() - 1; i++) {
-			for (int j = 0; j < list.size() - 1 - i; j++) {
-				MostRunsCSV mostRuns1 = list.get(j);
-				MostRunsCSV mostRuns2 = list.get(j + 1);
-				if (runsComparator.compare(mostRuns1, mostRuns2) < 0) {
-					list.set(j, mostRuns2);
-					list.set(j + 1, mostRuns1);
-				}
-			}
-		}
-		// to print list after sorting
-		for (int m = 0; m < list.size(); m++) {
-			System.out.println(list.get(m));
-		}
 	}
 
 	public String sortBatsmanDataOnStrikeRate() throws IplAnalyzerException {
@@ -103,7 +93,59 @@ public class IPLAnalyzer {
 		this.sortMostRunsCSV(csvComparator2);
 		String sortedRunsList = new Gson().toJson(list);
 		return sortedRunsList;
+	}
 
+	public String sortDataAccordingToSortType(SortType sortType) throws IplAnalyzerException {
+		if (list == null || list.size() == 0) {
+			throw new IplAnalyzerException("No Data Found", IplAnalyzerException.ExceptionType.NO_DATA_FOUND);
+		}
+		this.sortMostRunsCSV(sortedMap.get(sortType).reversed());
+		String sortedStateCensus = new Gson().toJson(list);
+		return sortedStateCensus;
+	}
+
+	private void sortMostRunsCSV(Comparator<MostRunsCSV> runsComparator) {
+		for (int i = 0; i < list.size() - 1; i++) {
+			for (int j = 0; j < list.size() - 1 - i; j++) {
+				MostRunsCSV mostRuns1 = list.get(j);
+				MostRunsCSV mostRuns2 = list.get(j + 1);
+				if (runsComparator.compare(mostRuns1, mostRuns2) < 0) {
+					list.set(j, mostRuns2);
+					list.set(j + 1, mostRuns1);
+				}
+			}
+		}
+		// to print list after sorting
+		for (int m = 0; m < list.size(); m++) {
+			System.out.println(list.get(m));
+		}
+	}
+
+	private void sortMostWicketsCSV(Comparator<MostWicketsCSV> wicketsComparator) {
+		for (int i = 0; i < listBowler.size() - 1; i++) {
+			for (int j = 0; j < listBowler.size() - 1 - i; j++) {
+				MostWicketsCSV mostRuns1 = listBowler.get(j);
+				MostWicketsCSV mostRuns2 = listBowler.get(j + 1);
+				if (wicketsComparator.compare(mostRuns1, mostRuns2) < 0) {
+					listBowler.set(j, mostRuns2);
+					listBowler.set(j + 1, mostRuns1);
+				}
+			}
+		}
+		// to print list after sorting
+		for (int m = 0; m < listBowler.size(); m++) {
+			System.out.println(listBowler.get(m));
+		}
+	}
+
+	public String sortBowlerDataOnAverage() throws IplAnalyzerException {
+		if (listBowler == null || listBowler.size() == 0) {
+			throw new IplAnalyzerException("No Data Found in list", IplAnalyzerException.ExceptionType.NO_DATA_FOUND);
+		}
+		Comparator<MostWicketsCSV> csvComparator = Comparator.comparing(player -> Double.parseDouble(player.average));
+		this.sortMostWicketsCSV(csvComparator);
+		String sortedWicketsList = new Gson().toJson(listBowler);
+		return sortedWicketsList;
 	}
 
 }
--- test/java/com/ipl/IPLAnalysisTest.java
+++ test/java/com/ipl/IPLAnalysisTest.java
@@ -9,6 +9,7 @@ import com.google.gson.Gson;
 public class IPLAnalysisTest {
 
 	public static String FILEPATH = "C:\\Users\\sippo\\eclipse-workspace\\IPL\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+	public static String FILEPATHBOWLER = "C:\\Users\\sippo\\eclipse-workspace\\IPL\\WP DP Data_02 IPL2019FactsheetMostWkts.csv";
 	private IPLAnalyzer iplAnalyzer;
 
 	@Before
@@ -16,6 +17,7 @@ public class IPLAnalysisTest {
 		iplAnalyzer = new IPLAnalyzer();
 	}
 
+	// UC1
 	@Test
 	public void givenCSVFILEPATH_ShouldReturnTopBattingAverage() throws IplAnalyzerException {
 		iplAnalyzer.loadIplData(FILEPATH);
@@ -25,6 +27,7 @@ public class IPLAnalysisTest {
 		Assert.assertEquals("83.2", mostRunsCSV[0].average);
 	}
 
+	// UC2
 	@Test
 	public void givenCSVFILEPATH_ShouldReturnTopStrikeRate() throws IplAnalyzerException {
 		iplAnalyzer.loadIplData(FILEPATH);
@@ -34,8 +37,9 @@ public class IPLAnalysisTest {
 		Assert.assertEquals("333.33", mostRunsCSV[0].strikeRate);
 	}
 
+	// UC3
 	@Test
-	public void givenCSVFILEPATH_ShouldReturnPlayerWithMostSixesAndFours() throws IplAnalyzerException {
+	public void givenCSVFILEPATH_ShouldReturnPlayerWithMostSixesNFours() throws IplAnalyzerException {
 		iplAnalyzer.loadIplData(FILEPATH);
 		String sortedData = iplAnalyzer.sortBatsmanDataOnBoundaries();
 		MostRunsCSV[] mostRunsCSV = new Gson().fromJson(sortedData, MostRunsCSV[].class);
@@ -43,6 +47,7 @@ public class IPLAnalysisTest {
 		Assert.assertEquals(83, mostRunsCSV[0].foursCollected + mostRunsCSV[0].sixesCollected);
 	}
 
+	// UC4
 	@Test
 	public void givenCSVFILEPATH_ShouldReturnPlayerWithBestStrikeAndBoundaries() throws IplAnalyzerException {
 		iplAnalyzer.loadIplData(FILEPATH);
@@ -51,19 +56,33 @@ public class IPLAnalysisTest {
 		Assert.assertEquals("Andre Russell", mostRunsCSV[0].playerName);
 	}
 
+	// UC5
 	@Test
-	public void givenCSVFilePath_ShouldReturnPlayerWithBestAverageWithStrikeRate() throws IplAnalyzerException {
+	public void givenCSVFILEPATH_ShouldReturnPlayerWithBestAverageWithStrikeRate() throws IplAnalyzerException {
 		iplAnalyzer.loadIplData(FILEPATH);
 		String sortedData = iplAnalyzer.sortBatsmanDataOnStrikeRateThenAverage();
 		MostRunsCSV[] mostRunsCSV = new Gson().fromJson(sortedData, MostRunsCSV[].class);
 		Assert.assertEquals("Ishant Sharma", mostRunsCSV[0].playerName);
 	}
-	
+
+	// UC6
 	@Test
-	public void givenCSVFilePath_ShouldReturnPlayerWithMaximumRunsWithBestAverages() throws IplAnalyzerException {
+	public void givenCSVFILEPATH_ShouldReturnPlayerWithMaximumRunsWithBestAverages() throws IplAnalyzerException {
 		iplAnalyzer.loadIplData(FILEPATH);
 		String sortedData = iplAnalyzer.sortBatsmanDataOnAverageThenMaximumRuns();
 		MostRunsCSV[] mostRunsCSV = new Gson().fromJson(sortedData, MostRunsCSV[].class);
 		Assert.assertEquals("MS Dhoni", mostRunsCSV[0].playerName);
 	}
+
+	// UC7
+	@Test
+	public void givenCSVFILEPATH_ShouldReturnPalyersWithTopBowlingAverages() throws IplAnalyzerException {
+		iplAnalyzer.loadIplDataBowler(FILEPATHBOWLER);
+		String sortedData = iplAnalyzer.sortBowlerDataOnAverage();
+		MostWicketsCSV[] mostWicketsCSV = new Gson().fromJson(sortedData, MostWicketsCSV[].class);
+		Assert.assertEquals("Krishnappa Gowtham", mostWicketsCSV[0].playerName);
+		Assert.assertEquals("166", mostWicketsCSV[0].average);
+
+	}
+
 }
